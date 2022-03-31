0613c2e79da2b3077d2f2d62845024ab0dbc69b9
Jun Allard <jun.allard@uci.edu>
2022-03-31 14:59:23 -0700

    commandline class selection

--- makeSmallGroups.py
+++ makeSmallGroups.py
@@ -1,16 +1,40 @@
-def makeSmallGroups(STUDENTLIST):
+def makeSmallGroups():
 
-    import pandas as pd
-    import xlsxwriter
     import random
-    from datetime import date
-    import drawSvg as draw
     import os
+    import sys
+    import xlsxwriter
+    import pandas as pd
+    import drawSvg as draw
+    from datetime import date
 
+    # Get command-line arguments
+    if len(sys.argv)>1:
+        courseNumber = sys.argv[1]
+    else:
+        courseNumber = 'M227C'
+
+    studentListFile = 'studentList_' + courseNumber + '.xlsx'
+    if courseNumber == 'M227C':
+        Teams_list = [
+            ['Door', [-120-5,-40-55,80,70], 3],
+            ['Window', [+55-5,-40-55,95,70], 3],
+            ['Lectern',[+60-5,120-55,90,70], 4],
+            ['Whiteboard',[-120-5,120-55,115,70], 3],
+            ['Center', [-20-5,40-55,90,70], 3]
+            ]
+    elif courseNumber == 'P50':
+        Teams_list = [
+            ['Front Door', [-120-5,-40-55,80,70], 3],
+            ['Back Door', [+55-5,-40-55,95,70], 3],
+            ['Lectern',[+60-5,120-55,90,70], 4],
+            ['Middle',[-120-5,120-55,115,70], 3],
+            ['Back Corner', [-20-5,40-55,90,70], 3]
+            ]
 
     # SHUFFLE STUDENTS
 
-    student_df = pd.read_excel(STUDENTLIST)
+    student_df = pd.read_excel(studentListFile)
     student_list = list(student_df['Students'])
 
     random.shuffle(student_list)
@@ -30,13 +54,7 @@ def makeSmallGroups(STUDENTLIST):
     # Team Door
     # Team Whiteboard Corner
 
-    Teams_list = [
-        ['Door', [-120-5,-40-55,80,70], 3],
-        ['Window', [+55-5,-40-55,95,70], 3],
-        ['Lectern',[+60-5,120-55,90,70], 4],
-        ['Whiteboard',[-120-5,120-55,115,70], 3],
-        ['Center', [-20-5,40-55,90,70], 3]
-        ]
+
     Teams = pd.DataFrame(Teams_list, columns=['Name', 'Coords', 'NumberOfThinkers'])
 
     d = draw.Drawing(300, 350, origin='center', displayInline=False)
@@ -62,6 +80,9 @@ def makeSmallGroups(STUDENTLIST):
             12,thisTeam['Coords'][0]+5,thisTeam['Coords'][1]+55-10*(iStudent_inThisTeam+1),fill='black')
             iStudent = iStudent + 1
             d.append(studentLabel)
+            if iStudent > len(student_list):
+                break
+
     
 
     d.saveSvg('teams.svg')
@@ -70,6 +91,4 @@ def makeSmallGroups(STUDENTLIST):
 
 
 if __name__ == '__main__':
-    # write the folder containing input data. Output data will be written to same folder.
-    STUDENTLIST = 'studentList.xlsx'  # EDIT FOLDERNAME HERE
-    makeSmallGroups(STUDENTLIST)
+    makeSmallGroups()
