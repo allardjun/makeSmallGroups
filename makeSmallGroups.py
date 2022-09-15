163df586103eb29f57eb909973ca7ea574a674f8
Jun Allard <jun.allard@uci.edu>
2022-09-15 16:50:30 -0700

    set up for P230 22F

--- makeSmallGroups.py
+++ makeSmallGroups.py
@@ -39,7 +39,18 @@ def makeSmallGroups():
             ['Middle',      [125, yShift-115, 117, 70], 4]
             ]
         datepos = [5, 20]
-
+    elif courseNumber == 'P230':
+        ySize = 200
+        yShift = ySize/2+20
+        Teams_list = [
+            ['Whiteboard',   [200, yShift- 30+12, 117, 70], 4],
+            ['Door',         [  5, yShift-115+12, 117, 70], 4],
+            ['Window',       [250, yShift-115+12, 117, 70], 4],
+            ['Lectern',      [ 35, yShift- 30+12, 117, 70], 4],
+            ['Center',    [125, yShift-115+12, 117, 70], 4]
+            ]
+        datepos = [5, 20]
+``
 
     # SHUFFLE STUDENTS
 
