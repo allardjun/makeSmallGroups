9015abfd4ef92256b4379e1b10af84e30ac8c124
Jun Allard <jun.allard@uci.edu>
2022-11-12 11:52:40 -0800

    simplify coords and make P230 layout B

--- makeSmallGroups.py
+++ makeSmallGroups.py
@@ -16,16 +16,21 @@ def makeSmallGroups():
 
     emSize = 12
 
+    layout = 'B'
+
+    print((courseNumber == 'P230' and layout == 'B'))
+
+    # rectangle positions are specified by bottom left corner
+
     studentListFile = 'studentList_' + courseNumber + '.xlsx'
     if courseNumber == 'M227C':
         ySize = 200
-        yShift = ySize/2+20
         Teams_list = [
-            ['Whiteboard',   [200, yShift- 30+12, 117, 70], 4],
-            ['Door',         [  5, yShift-115+12, 117, 70], 4],
-            ['Window',       [250, yShift-115+12, 117, 70], 4],
-            ['Lectern',      [ 35, yShift- 30+12, 117, 70], 4],
-            ['Projector',    [125, yShift-115+12, 117, 70], 4]
+            ['Whiteboard',   [200, 102, 117, 70], 4],
+            ['Door',         [  5,  17, 117, 70], 4],
+            ['Window',       [250,  17, 117, 70], 4],
+            ['Lectern',      [ 35, 102, 117, 70], 4],
+            ['Projector',    [125,  17, 117, 70], 4]
             ]
         datepos = [5, 20]
     elif courseNumber == 'P50':
@@ -39,18 +44,30 @@ def makeSmallGroups():
             ['Middle',      [125, yShift-115, 117, 70], 4]
             ]
         datepos = [5, 20]
+    elif (courseNumber == 'P230' and layout == 'B'):
+        print(layout)
+        ySize = 350
+        yShift = ySize/2+70
+        Teams_list = [
+            ['Door',          [ 15,  57, 120], 3],
+            ['Clock',         [ 15, 142, 117], 3],
+            ['Whiteboards',   [ 15, 227, 121], 4],
+            ['Motion Sensor', [180,  57, 135], 3],
+            ['Metal Disk',    [180, 142, 117], 3],
+            ['Lectern',       [180, 227, 117], 4]
+            ]
+        datepos = [5, 20]
     elif courseNumber == 'P230':
         ySize = 350
         yShift = ySize/2+70
         Teams_list = [
-            ['Lectern',      [200, yShift- 30+12, 117, 80], 5],
-            ['Door',         [ 35, yShift-200+12, 117, 70], 4],
-            ['Window',       [200, yShift-200+12, 117, 70], 4],
-            ['Whiteboard',   [ 35, yShift- 30+12, 117, 70], 4],
-            ['Center',       [125, yShift-115+12, 117, 70], 4]
+            ['Lectern',      [200, 227, 117, 80], 5],
+            ['Door',         [ 35,  57, 117, 70], 4],
+            ['Window',       [200,  57, 117, 70], 4],
+            ['Whiteboard',   [ 35, 227, 117, 70], 4],
+            ['Center',       [125, 142, 117, 70], 4]
             ]
         datepos = [5, 20]
-
 
     # SHUFFLE STUDENTS
 
@@ -83,7 +100,7 @@ def makeSmallGroups():
                 thisTeam['Coords'][0],
                 thisTeam['Coords'][1],
                 thisTeam['Coords'][2],
-                thisTeam['Coords'][3],
+                (thisTeam['NumberOfThinkers']+1)*emSize+5,#thisTeam['Coords'][3],
                 fill='#ffffff',stroke_width=2,stroke='black')
 
             groupLabel = draw.Text('Team ' + thisTeam['Name'],
@@ -92,7 +109,10 @@ def makeSmallGroups():
             d.append(groupLabel)
             for iStudent_inThisTeam in range(thisTeam['NumberOfThinkers']):
                 studentLabel = draw.Text(student_list[iStudent],
-                12,thisTeam['Coords'][0]+5,thisTeam['Coords'][1]+emSize*thisTeam['NumberOfThinkers']+5-emSize*(iStudent_inThisTeam+1),fill='black')
+                    12,
+                    thisTeam['Coords'][0]+5,
+                    thisTeam['Coords'][1]+emSize*thisTeam['NumberOfThinkers']+5-emSize*(iStudent_inThisTeam+1),
+                    fill='black')
                 iStudent = iStudent + 1
                 d.append(studentLabel)
                 if iStudent >= len(student_list):
